4fe870ebbb14b961161cbed8307d7fb1fd3c9d8d
Richard <a983974247@gmail.com>
2021-02-02 21:57:46 +0800

    Add github Actions

--- next.config.js
+++ next.config.js
@@ -4,9 +4,15 @@ const withSass = require("@zeit/next-sass");
 const webpack = require("webpack");
 const path = require("path");
 
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = withPlugins([[withSass], [withImages]], {
   webpack(config, options) {
     config.resolve.modules.push(path.resolve("./"));
     return config;
-  }
+  },
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd
+    ? "https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/"
+    : "",
 });
